9e810f7bc21d94f58478c4b28e82fa4518c5c45d
Daniel <dkuo7@gatech.edu>
2014-09-19 14:43:37 -0400

    Test Code
    Test code for Player class.

---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Player player1 = new Player ("Daniel Kuo");
+		player1.selectRace ("human");
+		player1.selectWizard ("Wizard1");
+		player1.selectBanner (1);
+		player1.selectSpecialAbility ("Invinsible");
+		player1.intializeSpellRank ();
+		player1.setSpellRank (10);
+		player1.selectCityName ("Atlanta");
+		Debug.Log(player1.getRace());
+		Debug.Log(player1.getName());
+		Debug.Log (player1.getWizard());
+		Debug.Log (player1.getSpellRank ());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
