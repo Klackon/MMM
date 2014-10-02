16c2b2095e3da9a44aeca34dd5c191df5ae59312
davidscz <zzy3@gatech.edu>
2014-10-02 13:36:49 -0400

    Anyone pls fix
    public string name;
    public string race;
    public string wizard;
    public string portrait;
    public string spellRank;
    public string spellPick;
    public string specialAbility;
    public string banner;
    public string homeCityName;
    add all of this in textfield
    
    and I mess up the build order. someone help pls

--- Assets/Scripts/CharCreateScreen.cs
+++ Assets/Scripts/CharCreateScreen.cs
@@ -2,12 +2,34 @@
 using System.Collections;
 
 public class CharCreateScreen : MonoBehaviour {
-
+	public string name = "name";
+	public string race = "race";
+	public string wizard = "Select wizard";
+	public string portrait = "Select Portrait";
+	public string spellRank = "spellRank";
+	public string spellPick = "spellPick";
+	public string specialAbility = "speicalAbility";
+	public string banner = "banner";
+	public string homeCityName = "homeCityName";
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	void OnGUI() {
+		addOnGUI (name, 20);
+		addOnGUI (race, 40);
+		addOnGUI (wizard, 60);
+		addOnGUI (portrait, 80);
+		addOnGUI (spellRank, 100);
+		addOnGUI (spellPick, 120);
+		addOnGUI (specialAbility, 140);
+		addOnGUI (banner, 160);
+		addOnGUI (homeCityName, 180);
+	}
+
+	void addOnGUI(string name, int yaxis){
+		name = GUI.TextField(new Rect(60, yaxis, 100, 20), name, 25);
+		}
 	// Update is called once per frame
 	void Update () {
 		// switch to game
