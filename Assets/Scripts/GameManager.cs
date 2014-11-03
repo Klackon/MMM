fa36b370b1d125ab8949c2b811e794ff8ff2396a
cthildebrant <childebrant8@gmail.com>
2014-11-03 14:29:37 -0500

    game board

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	private ImmediateStateMachine stateMachine = new ImmediateStateMachine ();
+	//GameObject[,] board;
+	//public GameObject[,] coordinates;
+	public GameTile[,] board;
+	public int boardSize = 20;
+	int spaceOffset = 10;
 	int n;
 	GameObject player;
 	PlayerManager playerScript;
@@ -13,107 +19,54 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		n = 0;
+		float x = 5;
+		float y = -5;
+		float z = -10;
+
+		board = new GameTile[boardSize, boardSize];
+		//coordinates = new GameObject[boardSize, boardSize];
+		/*
+		for(int i = 0; i < boardSize; i++){
+			for(int j = 0; j < boardSize; j++){
+				coordinates[i, j] = new GameObject();
+				//coordinates[i, j].AddComponent("Transform");
+				coordinates[i, j].transform.position = new Vector2(i*spaceOffset,j*spaceOffset);
+			}
+		}
+		*/
+		for(int i=0; i<boardSize; i++){
+			for(int j=0; j<boardSize; j++){
+				//Vector2 pos = new Vector2(x, y);
+				Vector3 pos = new Vector3(x, y, z);
+				board[i, j] = new GameTile(pos);
+				x+=10;
+				if(j==boardSize-1){
+					x = 5;
+				}
+			}
+			y-=10;
+		}
+
+		/*
+		foreach(GameTile gt in board){
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = gt.position;
+			cube.transform.localScale = new Vector3(10,10,1);
+		}
+		*/
+
 		switchToGameFSM();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*
-		n++;
-		Debug.Log (n);
-		if(Input.GetKeyDown(KeyCode.C)){
-			Debug.Log("test");
-		}
-
-		if(Input.GetKey(KeyCode.D)){
-			Debug.Log ("D?");
-		}
-		*/
-
-		/*
-		if(Input.GetKeyDown(KeyCode.O)){
-			Application.Load
-		}
-		*/
-
+	
 		stateMachine.Execute();
 	}
 
 	//////////////////////////////////////////////////////////////////////
 	// GAME STATES
 	//////////////////////////////////////////////////////////////////////
-
-	/*
-	// Menu State	
-	public void switchToMenuFSM() {
-		stateMachine.ChangeState (enterMENU, updateMENU, exitMENU);
-	}
-	
-	void enterMENU() {
-		Application.LoadLevel(0);
-		Debug.Log("ENTER MENU");
-	}
-
-	void updateMENU() {
-		Debug.Log("MENU UPDATE");
-
-		if(Input.GetKeyDown(KeyCode.C)){
-			switchToCharCreateFSM();
-		}
-
-		if(Input.GetKeyDown(KeyCode.L)){
-			switchToLoadFSM();
-		}
-	}
-
-	void exitMENU () {
-
-	}
-
-	// Character Creation State	
-	public void switchToCharCreateFSM() {
-		stateMachine.ChangeState (enterCHARCREATE, updateCHARCREATE, exitCHARCREATE);
-	}
-	
-	void enterCHARCREATE() {
-		Application.LoadLevel(2);
-		Debug.Log("ENTER CHAR CREATE");
-	}
-	void updateCHARCREATE() {
-		if(Input.GetKeyDown(KeyCode.G)){
-			switchToGameFSM();
-		}
-
-		if(Input.GetKeyDown(KeyCode.M)){
-			switchToMenuFSM();
-		}
-	}
-	void exitCHARCREATE () {
-		
-	}
-
-	// Load State	
-	public void switchToLoadFSM() {
-		stateMachine.ChangeState (enterLOAD, updateLOAD, exitLOAD);
-	}
-	
-	void enterLOAD() {
-		Application.LoadLevel(5);
-		Debug.Log("ENTER LOAD");
-	}
-	void updateLOAD() {
-		if(Input.GetKeyDown(KeyCode.M)){
-			switchToMenuFSM();
-		}
-
-		if(Input.GetKeyDown(KeyCode.G)){
-			switchToGameFSM();
-		}
-	}
-	void exitLOAD () {
-		
-	}
-	*/
 
 	// Main Game State	
 	public void switchToGameFSM() {
@@ -128,11 +81,15 @@ public class GameManager : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag("Player");
 		if(player != null){
 			playerScript = player.GetComponent<PlayerManager>();
+			Debug.Log ("Player found");
+		} else {
+			Debug.Log ("ERROR: No Player Found");
 		}
 		enemy = GameObject.FindGameObjectsWithTag("Enemy");
 	}
 	void updateGAME() {
 
+		//Temporary input command for testing purposes
 		if(Input.GetKeyDown(KeyCode.Return)){
 			switchToPlayerTurnFSM();
 		}
@@ -190,6 +147,13 @@ public class GameManager : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Escape)) Application.LoadLevel(0);
 		
 		//LOGIC FOR GAMEPPLAY HERE
+
+		//board test
+		/*
+		foreach(GameObject go in coordinates){
+			player.transform.position = go.transform.position;
+		}
+		*/
 		
 		if(Input.GetKeyDown(KeyCode.Return)){
 			switchToEnemyTurnFSM();
---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTile{
+	public bool visited;
+	public enum TerrainType{Desert, Forest, Grasslands, Hills, Mountain, Ocean, River, Shores, Swamp, Tundra};
+	public TerrainType terrain;
+	public enum SpecialTerrain{ChaosNodes, NatureNodes, Ore, Ruins, Temples, SorceryNodes, TowerOfWizadry, UndergroundLair, Crystal, None};
+	public SpecialTerrain altTerrain;
+	public enum StructureType{Road, Cities, Hamlets, Settlements, None};
+	public StructureType structure; 
+	public bool hasStructure;
+	public bool hasSpecial;
+	public bool hasArmy;
+	public bool hasMonster;
+	public bool occupied;
+	public bool onScreen;
+	public bool revealed;
+	//public Vector2 position;
+	public Vector3 position;
+	public float movementVal;
+	public float buildVal;
+	public float foodProduction;
+	public float goldProduction;
+	public float manaProduction;
+	public float maxPopulation;
+
+	public GameTile(){
+		//position = Vector2.zero;
+		//position = new Vector2(5,-5);
+		position = new Vector3(5, -5, -10);
+		//position for first cube
+			// every subsequent square on this row is +10,0
+			// every subsequent row is 0, -10
+		movementVal = 1;
+		buildVal = 1;
+		foodProduction = 1;
+		goldProduction = 1;
+		manaProduction = 1;
+		maxPopulation = 1;
+		terrain = TerrainType.Desert;
+		altTerrain = SpecialTerrain.None;
+		structure = StructureType.None;
+		hasSpecial = false;
+		hasStructure = false;
+		hasArmy = false;
+		hasMonster = false;
+		occupied = false;
+		onScreen = false;
+		revealed = false;
+		createTile();
+	}
+
+	//public GameTile(Vector2 position){
+	public GameTile(Vector3 position){
+		this.position = position;
+		movementVal = 1;
+		buildVal = 1;
+		foodProduction = 1;
+		goldProduction = 1;
+		manaProduction = 1;
+		maxPopulation = 1;
+		terrain = TerrainType.Desert;
+		altTerrain = SpecialTerrain.None;
+		structure = StructureType.None;
+		hasSpecial = false;
+		hasStructure = false;
+		hasArmy = false;
+		hasMonster = false;
+		occupied = false;
+		onScreen = false;
+		revealed = false;
+		createTile();
+	}
+
+	// Adds the physical representation of the game tile
+	public GameObject createTile(){
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = this.position;
+		cube.transform.localScale = new Vector3(10,10,1);
+		return cube;
+	}
+
+	/*
+	public void setOnScreen(bool val){
+		onScreen = val;
+	}
+	public bool getOnScreen(){
+		return onScreen;
+	}
+
+	public void setRevealed(bool val){
+		revealed = val;
+	}
+	public bool getRevealed(){
+		return revealed;
+	}
+	*/
+
+	public bool hasFog(){
+		if(onScreen&&revealed){
+			return false;
+		} else if(onScreen&&!revealed){
+			return true;
+		} else {
+			return true;
+		}
+	}
+}
