0ecd5534851d93abb8e3ebd7188aadcb3e38a9c5
cthildebrant <childebrant8@gmail.com>
2014-11-27 16:52:11 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Scripts/GameTile.cs

---
+++ Assets/Location.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location{
+	int[] index = new int[2]; 
+	public GameObject gameCam;
+	public int x;
+	public int y;
+	//public GameBoard board;
+	public GameTile tile;
+
+	public Location(int i, int j){
+		index = new int[2];
+		index[0] = i;
+		index[1] = j;
+		y = i;
+		x = j;
+	}
+
+	public int[] getIndex(){
+		return index;
+	}
+
+	public void setTile(GameTile gt){
+		tile = gt;
+	}
+
+	public GameTile getTile(){
+		return tile;
+	}
+}
